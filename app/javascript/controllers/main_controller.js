b6b976d66ce3eaa24f43934cd07095a13227ea32
koki0317 <80045896+koki0317@users.noreply.github.com>
2022-09-04 15:55:19 +0900

    Added the my introduction

--- app/javascript/controllers/main_controller.js
+++ app/javascript/controllers/main_controller.js
@@ -2,6 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="main"
 export default class extends Controller {
+  static targets = ["navMenu", "skillsContent"]
+
   connect() {
+    console.log("Hello")
   }
+
+  navToggle() {
+    this.navMenuTarget.classList.add("show-menu")
+  }
+
+  navClose() {
+    this.navMenuTarget.classList.remove("show-menu")
+  }
+
+  //Accordion
+  toggleSkills() {
+    console.log(this.skillsContentTarget)
+  }
+
 }
--- app/javascript/main.js
+++
@@ -1,9 +0,0 @@
-const navMenu = document.getElementById("nav-menu"),
-      navToggle = document.getElementById("nav-toggle"),
-      navClose = document.getElementById("nav-close")
-
-      if(navToggle) {
-        navToggle.addEventListener("click", () => {
-          navMenu.classList.add("show-menu")
-        })
-      }
