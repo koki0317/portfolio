598b5588c21debeab5a56e12dc75d3ebde8fc072
koki0317 <80045896+koki0317@users.noreply.github.com>
2022-09-04 18:12:12 +0900

    Be able to collapse and open the skill list

--- app/javascript/controllers/main_controller.js
+++ app/javascript/controllers/main_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="main"
 export default class extends Controller {
-  static targets = ["navMenu", "skillsContent"]
+  static targets = ["navMenu", "skillsContentOpen", "skillsContentClose"]
 
   connect() {
     console.log("Hello")
@@ -17,8 +17,28 @@ export default class extends Controller {
   }
 
   //Accordion
-  toggleSkills() {
-    console.log(this.skillsContentTarget)
+  toggleSkillsOpen() {
+    // let itemClass = this.skillsContentOpenTarget.classList.value
+    // console.log(itemClass)
+    // if (itemClass === "skills__content skills__open") {
+    //   console.log("close")
+    //   itemClass = "skills__content skills__close"
+    // } else {
+    //   console.log("open")
+    //   itemClass = "skills__content skills__open"
+    // }
+    if (this.skillsContentOpenTarget.classList.value === "skills__content skills__open") {
+      return this.skillsContentOpenTarget.classList.value = "skills__content skills__close"
+    } else {
+      return this.skillsContentOpenTarget.classList.value = "skills__content skills__open"
+    }
   }
 
+  toggleSkillsClose() {
+    if (this.skillsContentCloseTarget.classList.value === "skills__content skills__close") {
+      return this.skillsContentCloseTarget.classList.value = "skills__content skills__open"
+    } else {
+      return this.skillsContentCloseTarget.classList.value = "skills__content skills__close"
+    }
+  }
 }
